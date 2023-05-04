ca6775987186b3b8bf15d3bc787f78b67f37a6e9
champignoom <champignoom@gmail.com>
2023-05-04 18:45:16 +0800

    current version: 2023.04.27 17:04

--- tex/texmf-context/tex/generic/context/luatex/luatex-fonts-merged.lua
+++ tex/texmf-context/tex/generic/context/luatex/luatex-fonts-merged.lua
@@ -1,6 +1,6 @@
 -- merged file : c:/data/develop/context/sources/luatex-fonts-merged.lua
 -- parent file : c:/data/develop/context/sources/luatex-fonts.lua
--- merge date  : 2023-04-15 00:23
+-- merge date  : 2023-04-27 17:04
 
 do -- begin closure to overcome local limits and interference
 
